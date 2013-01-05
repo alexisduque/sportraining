32bd6a5823ed66523d5058b91f273827eec77596
alexisd61@gmail.com <alexisd61@gmail.com>
2013-01-05 10:00:25 +0100

    sidebar sans accents

--- lib/sidebar.php
+++ lib/sidebar.php
@@ -21,17 +21,17 @@ $dateDuJour = $jour[date("w")]." ".date("d")." ".$mois[date("n")]." ".date("Y");
 
 			$reponse = mysql_query ('SELECT user.user , SUM(effectue.Duree) AS maxdureenatation FROM `effectue` JOIN user ON effectue.User_Id = user.id JOIN seances ON effectue.Seance_Id = seances.id AND seances.Sport LIKE "Natation" AND MONTH(Date) LIKE '.date("m").' GROUP BY User_Id ORDER BY MAX(Duree) DESC');?>
 <?php		if ($reponse != False) $donnees = mysql_fetch_array($reponse); ?>
-		<li><?php echo ('Le nageur du mois est '.htmlspecialchars($donnees['user']). ' qui a pass� ' .htmlspecialchars($donnees['maxdureenatation']).' minutes dans l\'eau') ;?></li>
+		<li><?php echo ('Le nageur du mois est '.htmlspecialchars($donnees['user']). ' qui a passé ' .htmlspecialchars($donnees['maxdureenatation']).' minutes dans l\'eau') ;?></li>
 <?php 		
 
 			$reponse = mysql_query ('SELECT user.user , SUM(effectue.Duree) AS maxdureecyclisme FROM `effectue` JOIN user ON effectue.User_Id = user.id JOIN seances ON effectue.Seance_Id = seances.id AND seances.Sport LIKE "Cyclisme" AND MONTH(Date) LIKE '.date("m").' GROUP BY User_Id ORDER BY MAX(Duree) DESC');?>
 <?php		if ($reponse != False) $donnees = mysql_fetch_array($reponse); ?>
-			<li><?php echo ('Notre champion cycliste du mois de '.date('F').' est '.htmlspecialchars($donnees['user']). ' qui a pass� ' .htmlspecialchars($donnees['maxdureecyclisme']).' minutes sur sa selle, il doit avoir sacr�ment mal au cul!') ;?></li>
+			<li><?php echo ('Notre champion cycliste du mois de '.date('F').' est '.htmlspecialchars($donnees['user']). ' qui a passe ' .htmlspecialchars($donnees['maxdureecyclisme']).' minutes sur sa selle, il doit avoir sacrement mal au cul!') ;?></li>
 <?php 		
 
 			$reponse = mysql_query ('SELECT user.user , SUM(effectue.Duree) AS maxdureecourse FROM `effectue` JOIN user ON effectue.User_Id = user.id JOIN seances ON effectue.Seance_Id = seances.id AND seances.Sport LIKE "Course" AND MONTH(Date) LIKE '.date("m").' GROUP BY User_Id ORDER BY MAX(Duree) DESC');?>
 <?php		if ($reponse != False) $donnees = mysql_fetch_array($reponse); ?>
-			<li><?php echo (htmlspecialchars($donnees['user']). ' est celui qui a us� le plus ces chaussures en '.date('F').' avec ' .htmlspecialchars($donnees['maxdureecourse']).' minutes de course � pied') ;?></li>
+			<li><?php echo (htmlspecialchars($donnees['user']). ' est celui qui a usé le plus ces chaussures en '.date('F').' avec ' .htmlspecialchars($donnees['maxdureecourse']).' minutes de course a pied') ;?></li>
 				
 
 		</ul>
